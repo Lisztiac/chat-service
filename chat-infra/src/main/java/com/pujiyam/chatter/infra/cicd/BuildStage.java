b51da5a8b4a929ca69e926334a4978f082476482
Lisztiac <landan.op104@gmail.com>
2024-06-08 13:15:57 -0400

    test another approach

--- chat-infra/src/main/java/com/pujiyam/chatter/infra/cicd/BuildStage.java
+++ chat-infra/src/main/java/com/pujiyam/chatter/infra/cicd/BuildStage.java
@@ -4,6 +4,8 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.Stage;
 import software.amazon.awscdk.StageProps;
 import software.amazon.awscdk.services.codebuild.*;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.Repository;
 import software.constructs.Construct;
 
 import java.util.List;
@@ -18,27 +20,25 @@ public class BuildStage extends Stage {
     public BuildStage(final Construct scope, final String id, final StageProps props) {
         super(scope, id, props);
 
-        GitHubSourceProps ghProps = GitHubSourceProps.builder()
-                .owner("Lisztiac")
-                .repo("")
-                .webhookFilters(List.of(
-                        FilterGroup.inEventOf(EventAction.PUSH)
-                                .andBranchIs("main")))
+        IRepository imgRepo = Repository.fromRepositoryName(this, "ChatterRepo", "cdk-hnb659fds-container-assets-984235857022-us-east-1");
+
+        BuildEnvironment buildEnv = BuildEnvironment.builder()
+                .buildImage(LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_3_0)
+                .privileged(true)
+                .environmentVariables(Map.of(
+                        "AWS_REGION", buildVar(Stack.of(this).getRegion()),
+                        "AWS_ACCOUNT_ID", buildVar(Stack.of(this).getAccount()),
+                        "IMAGE_REPO_URI", buildVar(imgRepo.getRepositoryUri())
+                ))
                 .build();
 
-        PipelineProject.Builder
+        PipelineProject project = PipelineProject.Builder
                 .create(this, "ChatterProject")
                 .buildSpec(BuildSpec.fromAsset("buildspec.yml"))
-                .environment(BuildEnvironment.builder()
-                        .buildImage(LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_3_0)
-                        .environmentVariables(Map.of(
-                                "AWS_DEFAULT_REGION", buildVar(Stack.of(this).getRegion()),
-                                "AWS_ACCOUNT_ID", buildVar(Stack.of(this).getAccount()),
-                                "IMAGE_REPO_NAME", buildVar(""),
-                                "IMAGE_REPO_URI", buildVar(""),
-                                "IMAGE_TAG", buildVar("latest")
-                        ))
-                        .build());
+                .environment(buildEnv)
+                .build();
+
+        imgRepo.grantPullPush(project.getGrantPrincipal());
     }
 
     public BuildEnvironmentVariable buildVar(Object value) {
--- chat-infra/src/main/java/com/pujiyam/chatter/infra/cicd/PipelineStack.java
+++ chat-infra/src/main/java/com/pujiyam/chatter/infra/cicd/PipelineStack.java
@@ -3,14 +3,8 @@ package com.pujiyam.chatter.infra.cicd;
 import software.amazon.awscdk.SecretValue;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.pipelines.CodeBuildStep;
-import software.amazon.awscdk.pipelines.CodePipeline;
-import software.amazon.awscdk.pipelines.CodePipelineSource;
-import software.amazon.awscdk.pipelines.ShellStep;
-import software.amazon.awscdk.services.codebuild.BuildEnvironment;
-import software.amazon.awscdk.services.codebuild.BuildEnvironmentVariable;
-import software.amazon.awscdk.services.codebuild.LinuxArmBuildImage;
-import software.amazon.awscdk.services.codebuild.Project;
+import software.amazon.awscdk.pipelines.*;
+import software.amazon.awscdk.services.codebuild.*;
 import software.amazon.awscdk.services.codepipeline.Artifact;
 import software.amazon.awscdk.services.codepipeline.Pipeline;
 import software.amazon.awscdk.services.codepipeline.StageProps;
@@ -53,22 +47,88 @@ public class PipelineStack extends Stack {
                         "IMAGE_REPO_URI", buildVar(imgRepo.getRepositoryUri())
                 )).build();
 
+        Artifact sourceOutput = new Artifact();
+        Artifact buildOutput = new Artifact();
+
+        Pipeline buildPipeline = Pipeline.Builder
+                .create(this, "BuildPipeline")
+                .restartExecutionOnUpdate(true)
+                .stages(List.of(
+                        createSourceStage("Source", sourceOutput),
+                        createImageBuildStage("ImageBuild", sourceOutput, buildOutput, imgRepo)
+                ))
+                .build();
+
         ShellStep synth = CodeBuildStep.Builder
                 .create("Synth")
-                .input(CodePipelineSource.gitHub("Lisztiac/chat-service", "main"))
+                .input(CodePipelineFileSet.fromArtifact(sourceOutput))
                 .buildEnvironment(buildEnv)
-                .commands(List.of("cd chat-infra", "build.sh"))
+                .commands(List.of(
+                        "cd chat-infra",
+                        "npm install -g aws-cdk",
+                        "cdk synth"
+                ))
                 .primaryOutputDirectory("chat-infra/cdk.out")
                 .build();
 
-        CodePipeline pipeline = CodePipeline.Builder
+        CodePipeline codePipeline = CodePipeline.Builder
                 .create(this, "ChatterPipeline")
-                .pipelineName("chatter-pipeline")
-                .dockerEnabledForSelfMutation(true)
+                .pipelineName("DeployPipeline")
+                .codePipeline(buildPipeline)
                 .synth(synth)
                 .build();
 
 //        pipeline.addStage(new EksStage(this, "eks-stage"));
+    }
+
+    private StageProps createSourceStage(String stageName, Artifact output) {
+        GitHubSourceAction action = GitHubSourceAction.Builder
+                .create()
+                .actionName("GitHubAction")
+                .owner("Lisztian")
+                .repo("chat-service")
+                .branch("main")
+                .oauthToken(SecretValue.secretsManager("github-token"))
+                .output(output)
+                .build();
+
+        return StageProps.builder()
+                .stageName(stageName)
+                .actions(List.of(action))
+                .build();
+    }
+
+    private StageProps createImageBuildStage(String stageName, Artifact input, Artifact output, IRepository imgRepo) {
+        BuildEnvironment buildEnv = BuildEnvironment.builder()
+                .buildImage(LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_3_0)
+                .privileged(true)
+                .environmentVariables(Map.of(
+                        "AWS_REGION", buildVar(Stack.of(this).getRegion()),
+                        "AWS_ACCOUNT_ID", buildVar(Stack.of(this).getAccount()),
+                        "IMAGE_REPO_URI", buildVar(imgRepo.getRepositoryUri())
+                ))
+                .build();
+
+        PipelineProject project = PipelineProject.Builder
+                .create(this, "ChatterProject")
+                .buildSpec(BuildSpec.fromAsset("buildspec.yml"))
+                .environment(buildEnv)
+                .build();
+
+        imgRepo.grantPullPush(project.getGrantPrincipal());
+
+        CodeBuildAction action = CodeBuildAction.Builder
+                .create()
+                .actionName("ImageBuildAction")
+                .project(project)
+                .input(input)
+                .outputs(List.of(output))
+                .build();
+
+        return StageProps.builder()
+                .stageName(stageName)
+                .actions(List.of(action))
+                .build();
     }
 
     private BuildEnvironmentVariable buildVar(Object value) {
